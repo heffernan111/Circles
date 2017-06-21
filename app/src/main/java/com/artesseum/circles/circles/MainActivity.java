0966fd184fae4c53150ded0fb8d5079b552ffa91
Unknown <david.heff.dh@googlemail.com>
2017-06-21 22:50:30 +0100

    First Upload. Day 1

---
+++ app/src/main/java/com/artesseum/circles/circles/MainActivity.java
@@ -0,0 +1,68 @@
+package com.artesseum.circles.circles;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        loginButton = (LoginButton)findViewById(R.id.fb_login_button);
+        textView = (TextView)findViewById(R.id.textView);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+               /* Log.d("Login_Success", "Success");
+                loginButton.setVisibility(textView.INVISIBLE);*/
+
+            }
+
+            @Override
+            public void onCancel() {
+
+                    textView.setText("Login Cancelled");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        if (resultCode == RESULT_OK){
+            Intent mainMenuIntent = new Intent(this, MainMenu.class);
+            startActivity(mainMenuIntent);
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/artesseum/circles/circles/MainMenu.java
@@ -0,0 +1,43 @@
+package com.artesseum.circles.circles;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainMenu extends AppCompatActivity {
+
+    public Button playGameButton;
+
+    public void init(){
+        playGameButton = (Button) findViewById(R.id.playButton);
+        playGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent gamePlay = new Intent(MainMenu.this, PlayGame.class);
+                startActivity(gamePlay);
+            }
+        });
+
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+            init();
+
+
+
+
+
+            }
+        }
+
