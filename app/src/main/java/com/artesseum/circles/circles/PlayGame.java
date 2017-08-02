d5fcca021167fd1dc78ed0f1b46dcd447cfaf66e
Unknown <david.heff.dh@googlemail.com>
2017-08-02 21:52:09 +0100

    THIS is beST 02/08/17

--- app/src/main/java/com/artesseum/circles/circles/PlayGame.java
+++ app/src/main/java/com/artesseum/circles/circles/PlayGame.java
@@ -4,7 +4,10 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -21,16 +24,19 @@ public class PlayGame extends AppCompatActivity {
     CountDownTimer timer;
     int time = 10;
     int Score = 0;
-
-
-
+    
 
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_playgame);
 
 
