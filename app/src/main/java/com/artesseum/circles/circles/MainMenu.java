a45bcdcaf3c865d434783df21e20e54027dbf6d7
Unknown <david.heff.dh@googlemail.com>
2017-08-07 18:54:59 +0100

    exit button

--- app/src/main/java/com/artesseum/circles/circles/MainMenu.java
+++ app/src/main/java/com/artesseum/circles/circles/MainMenu.java
@@ -1,6 +1,7 @@
 package com.artesseum.circles.circles;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,13 @@ import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
-    public Button playGameButton;
+    public Button playGameButton,exitButton;
 
     public void init(){
         playGameButton = (Button) findViewById(R.id.playButton);
+        exitButton = (Button) findViewById(R.id.exitButton);
+
+
         playGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,6 +25,17 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(gamePlay);
             }
         });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+
+            }
+        });
+
+
 
 
     }
